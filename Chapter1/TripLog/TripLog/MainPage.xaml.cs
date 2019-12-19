6e84d1a70e10afa5e0bb85f8d6b35c8ed6327438
Ed Snider <ed.snider@infernored.com>
2019-12-18 21:16:11 -0500

    chapter 1 - start

---
+++ Chapter1/TripLog/TripLog/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace TripLog
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
